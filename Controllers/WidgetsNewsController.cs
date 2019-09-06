04c59567927a1e4411e5cb94f441787ac2349184
Romulo AC <rcianci@gmail.com>
2019-09-06 00:14:42 -0300

    Migração para versão 4.20

--- Controllers/WidgetsNewsController.cs
+++ Controllers/WidgetsNewsController.cs
@@ -4,6 +4,8 @@ using Nop.Web.Framework.Controllers;
 using Nop.Web.Framework;
 using Microsoft.AspNetCore.Mvc;
 using Nop.Services.Localization;
+using Nop.Services.Security;
+using Nop.Services.Messages;
 
 namespace NopBrasil.Plugin.Widgets.News.Controllers
 {
@@ -13,17 +15,25 @@ namespace NopBrasil.Plugin.Widgets.News.Controllers
         private readonly ISettingService _settingService;
         private readonly NewsSettings _NewsSettings;
         private readonly ILocalizationService _localizationService;
+        private readonly INotificationService _notificationService;
+        private readonly IPermissionService _permissionService;
 
         public WidgetsNewsController(ISettingService settingService,
-            NewsSettings NewsSettings, ILocalizationService localizationService)
+            NewsSettings NewsSettings, ILocalizationService localizationService,
+            INotificationService notificationService, IPermissionService permissionService)
         {
             this._settingService = settingService;
             this._NewsSettings = NewsSettings;
             this._localizationService = localizationService;
+            this._notificationService = notificationService;
+            this._permissionService = permissionService;
         }
 
-        public ActionResult Configure()
+        public IActionResult Configure()
         {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageShippingSettings))
+                return AccessDeniedView();
+
             var model = new ConfigurationModel()
             {
                 WidgetZone = _NewsSettings.WidgetZone,
@@ -33,15 +43,18 @@ namespace NopBrasil.Plugin.Widgets.News.Controllers
         }
 
         [HttpPost]
-        public ActionResult Configure(ConfigurationModel model)
+        public IActionResult Configure(ConfigurationModel model)
         {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageShippingSettings))
+                return AccessDeniedView();
+
             if (!ModelState.IsValid)
                 return Configure();
 
             _NewsSettings.QtdNewsPosts = model.QtdNewsPosts;
             _NewsSettings.WidgetZone = model.WidgetZone;
             _settingService.SaveSetting(_NewsSettings);
-            SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+            _notificationService.SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
             return Configure();
         }
     }
